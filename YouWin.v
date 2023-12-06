58bb4d18a2adb143c673c97aaf8d15be65b5a097
sahilkhan2304 <150193116+sahilkhan2304@users.noreply.github.com>
2023-12-06 14:19:17 -0700

    Update YouWin.v
    95% done

--- YouWin.v
+++ YouWin.v
@@ -27,18 +27,20 @@ parameter BLACK = 12'b000000000000;
 
 reg [10:0] 	win_addr;
 wire [11:0] win_out;
-wire [3:0] 	pixel_dout_red;
-wire [3:0] 	pixel_dout_blue;
-wire [3:0] 	pixel_dout_green;
+wire [3:0] 	pixel_dout;
 
 // You win image position start points
-reg [9:0] win_x;     // location x start point
-reg [9:0] win_y;     // location y start point 
+reg [9:0] win_x = 260;     // location x start point
+reg [9:0] win_y = 232;     // location y start point 
 reg [9:0] index_x, index_y;
 
 
 // ROM for you win image
-
+you_win_mem you_win (
+  .clka(clk),    // input wire clka
+  .addra(win_addr),  // input wire [10 : 0] addra
+  .douta(pixel_dout)  // output wire [3 : 0] douta
+);
 
 
 // getting the x & y coordinates to position in ROM
@@ -48,7 +50,7 @@ always @(posedge clk) begin
 end
 
 always @(posedge clk) begin
-	if (score_in == 40) begin
+	if (score_in == 50) begin
 		win_reset_flag <= 1'b1;
 	end
 	else begin
@@ -58,19 +60,16 @@ end
 
 // Printing you win image when in boundary or else Black out.
 always @(posedge clk) begin
-	// 32 x 64 image (64 rows & 32 columns)
-	/*if ((index_x >= 0 && index_x < 32) && (index_y >= 0 && index_y < 64)) begin
-		win_addr <= {index_y[5:0], index_x[4:0]};
+	if ((index_x >= 0 && index_x < 97) && (index_y >= 0 && index_y < 16)) begin
+		win_addr <= {index_y[3:0], index_x[6:0]};
 		you_win_out <= win_out;
     end
     else begin
         you_win_out <= BLACK;
     end
-    */
-    you_win_out <= BLACK;
 end
 
 // Concatenate all colors
-//assign win_out = {pixel_dout_blue[3:0], pixel_dout_green[3:0], pixel_dout_red[3:0]};
+assign win_out = {pixel_dout, pixel_dout, pixel_dout};
 
 endmodule
